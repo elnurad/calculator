7b04d69c02b950ce9ad5c368b1a97e490397bc65
Elnura D <elnurab@gmail.com>
2021-12-08 17:48:54 +0800

    added functions

--- home.js
+++ home.js
@@ -1,16 +1,3 @@
-//functions for basic operations
-// const add =(a, b) => a + b;
-
-// const subtract = (a, b)=>a - b;
-
-// const multiply=(a, b)=> a * b;
-
-// const divide = (a, b) => a / b;
-
-// const operate = (a, operator, b) => operator(a, b)
-// operate(1, add 2) => 1+2
-
-// let result = eval(`${arr[0]}${arr[1]}${arr[2]}`)
 
 const calculation =(a, operator, b)=>{
     switch(operator){
@@ -23,197 +10,123 @@ const calculation =(a, operator, b)=>{
 }
 
 
-
 //display
 const display = document.querySelector('.display');
 console.log(display.innerHTML)
+
+const clear = document.querySelector('.clear')
+const clearDisplay=()=>{
+    clear.addEventListener('click', ()=> {
+        display.innerHTML = ''
+        displayValue = ''
+        console.log('clear')})
+    
+    
+}
 // const numberButtons = Array.from(document.getElementsByClassName('num_button'));
 const buttons = Array.from(document.getElementsByClassName('calculator'))
 
-let currentDisplayValue = ''
+let displayValue = ''
 
 
 let result = ''
 
-const updateDisplay =()=>{
-    buttons.forEach(button=>{
+
+
+
+let operatorIsExpected = false;
+
+const numberButtons = Array.from(document.getElementsByClassName('num-button'));
+
+
+const numbersOnScreen = ()=>{
+    numberButtons.forEach(button=>{
         button.addEventListener('click', function(){
             display.innerHTML += button.innerHTML;
-            currentDisplayValue = display.innerHTML;
+            displayValue = display.innerHTML;
+            operatorIsExpected = true;
+
+          
+            
+        })
+    })
+}
+numbersOnScreen()
 
 
-            if (button.innerHTML === '='){
-                const arr = currentDisplayValue.split(/\b/g)
-                while(arr[3]!=='='){
+
+const operatorButtons = Array.from(document.getElementsByClassName('operator'));
+const operatorsOnScreen = () =>{
+    operatorButtons.forEach(button=>{
+        button.addEventListener('click', (e)=>{
+            if (operatorIsExpected){
+                display.innerHTML += button.innerHTML;
+                displayValue = display.innerHTML
+                operatorIsExpected = false;
+
+            }
+            displayValue = displayValue
+            console.log(displayValue)
+ 
+        }
+    )
+    })
+}
+operatorsOnScreen()
+
+const equals = document.querySelector('.equal')
+
+const updateDisplay =()=>{
+    equals.addEventListener('click', ()=>{
+        if(displayValue.endsWith('=')){
+            displayValue = displayValue
+        }else{
+            display.innerHTML += equals.innerHTML;
+            displayValue = display.innerHTML
+        }
+        
+        
+
+        const arr = displayValue.split(/\b/g)
+        console.log(arr)
+        if(arr.length > 3){
+        while(arr[3]!=='='){
                     let result = calculation(parseInt(arr[0]), arr[1], parseInt(arr[2]))
                     arr.splice(0,3)
                     arr.unshift(result)
                     display.innerHTML = String(result)
-                   
+                    operatorIsExpected = false
                     console.log(result)
 
                 } 
                 if(arr[3]==='='){
                     let result = calculation(parseInt(arr[0]), arr[1], parseInt(arr[2]))
                     display.innerHTML = String(result)
+                    operatorIsExpected = false
                     console.log(result)
-                    
-
-                    
-
-                    
-
                 }
-           
-           
-
-
-
-          
-
+            }else{
+                display.innerHTML = ''
+                displayValue = ''
             }
-           
-            
-            
+
+
+            })}
+
+
+
     
-    
-        })
-    
-    })
-    };
 
 
-
-
-
-
-
-
-
-
-
-
-
-// const updateDisplay =()=>{
-//     buttons.forEach(button=>{
-//         button.addEventListener('click', function(){
-//             display.innerHTML += button.innerHTML;
-//             currentDisplayValue = display.innerHTML;
-
-
-//             if (button.innerHTML === '='){
-//                 const arr = currentDisplayValue.split(/\b/g)
-//                 while(arr[3]!=='='){
-//                     let result = eval(`${arr[0]}${arr[1]}${arr[2]}`)
-//                     arr.splice(0,3)
-//                     arr.unshift(result)
-//                     console.log(result)
-
-//                 } if(arr[3]==='='){
-//                     let result = eval(`${arr[0]}${arr[1]}${arr[2]}`)
-//                     console.log(result)
-
-//                 }
-                
-
-//             }
-//             console.log(currentDisplayValue)
-            
             
     
-    
-//         })
-    
-//     })
-//     };
 
 
-
-
-
-
-
-
-
-
-// const updateDisplay =()=>{
-//     buttons.forEach(button=>{
-//         button.addEventListener('click', function(){
-//             display.innerHTML += button.innerHTML;
-//             currentDisplayValue = display.innerHTML;
-//             if (button.innerHTML === '='){
-//                 const display_arr = currentDisplayValue.split(/\b/g)
-//                 if(display_arr[3] === '='){
-//                     let result =eval(`${display_arr[0]}${display_arr[1]}${display_arr[2]}`)
-//                     console.log(result)
-//                 }
-//                 else{
-//                     let result = eval(`${display_arr[0]}${display_arr[1]}${display_arr[2]}`)
-//                     display_arr.splice(0,3)
-//                     display_arr.unshift(result)
-//                     let final_result = eval(`${display_arr[0]}${display_arr[1]}${display_arr[2]}`)
-//                     console.log(final_result)
-
-//                 }
-                
-
-//             }
-//             console.log(currentDisplayValue)
-            
-            
-    
-    
-//         })
-    
-//     })
-//     };
-
-
-
-
-
-
-// const updateDisplay =()=>{
-// numberButtons.forEach(numButton=>{
-//     numButton.addEventListener('click', function(){
-//         display.innerHTML += numButton.innerHTML;
-//         currentDisplayValue = display.innerHTML;
-//         console.log(currentDisplayValue)
-        
-
-
-//     })
-
-// })
-// };
 updateDisplay()
 
-//operators
-// const operators = Array.from(document.getElementsByClassName('operator'))
-// console.log(operators[0].innerHTML)
 
 
 
-//Action on Operators
-// const operatorAction =()=>{
-//     operators.forEach(operator=>{
-//         operator.addEventListener('click', ()=>{ 
-//              display.innerHTML = operator.innerHTML
-//         })
-//     })
-// }
 
 
-//clear
-const clear = document.querySelector('.clear')
-const clearDisplay=()=>{
-    clear.addEventListener('click', ()=> {
-        display.innerHTML = ''
-        console.log('clear')})
-    
-    
-}
-    
-
-// operatorAction()
 clearDisplay()
